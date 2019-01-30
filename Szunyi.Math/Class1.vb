797eb875fde2c2001f744520f6136a0ac974895e
Attila Szűcs <a.szunyi@gmail.com>
2019-01-30 09:41:23 +0100

    a

--- Szunyi.Math/Class1.vb
+++ Szunyi.Math/Class1.vb
@@ -1,3 +1,39 @@
-﻿Public Class Class1
+﻿Public Class Operators
+    Public Property LogicalOperators As New List(Of LogicalOperator)
+    Public Property MathematicalOperators As New List(Of LogicalOperator)
+    Public Property AggregateOperators As New List(Of LogicalOperator)
+    Public Property BinaryOperators As New List(Of LogicalOperator)
+    Public Sub New()
+        LogicalOperators.Add(New LogicalOperator With {.Name = "AND", .Symbol = "&", .Description = "1,1 = True else False"})
+        LogicalOperators.Add(New LogicalOperator With {.Name = "OR", .Symbol = "|", .Description = "0,0 = False else True"})
+        LogicalOperators.Add(New LogicalOperator With {.Name = "XOR", .Symbol = "(+)", .Description = "0,1 and 1,0 = True else False"})
+        LogicalOperators.Add(New LogicalOperator With {.Name = "NOR", .Symbol = "~|", .Description = "0,0  = True else False"})
+        LogicalOperators.Add(New LogicalOperator With {.Name = "NAND", .Symbol = "~&", .Description = "1,1  = False else True"})
 
+        MathematicalOperators.Add(New LogicalOperator With {.Name = "Add", .Symbol = "+", .Description = "Addition"})
+        MathematicalOperators.Add(New LogicalOperator With {.Name = "Minus", .Symbol = "-", .Description = "Subtraction"})
+        MathematicalOperators.Add(New LogicalOperator With {.Name = "Multiply", .Symbol = "*", .Description = "Multiplication"})
+        MathematicalOperators.Add(New LogicalOperator With {.Name = "Division", .Symbol = "/", .Description = "Division"})
+        MathematicalOperators.Add(New LogicalOperator With {.Name = "Exponentiation", .Symbol = "^", .Description = "Exponentiation"})
+        MathematicalOperators.Add(New LogicalOperator With {.Name = "Factorial", .Symbol = "!", .Description = "Factorial"})
+        MathematicalOperators.Add(New LogicalOperator With {.Name = "Mod", .Symbol = "#", .Description = "Modulo function"})
+
+        AggregateOperators.Add(New LogicalOperator With {.Name = "Sum", .Symbol = "sum", .Description = "Summation operator"})
+        AggregateOperators.Add(New LogicalOperator With {.Name = "Average", .Symbol = "avg", .Description = "Average operator"})
+        AggregateOperators.Add(New LogicalOperator With {.Name = "Minimum value", .Symbol = "mini", .Description = "Minimum value operator"})
+        AggregateOperators.Add(New LogicalOperator With {.Name = "Maximum value", .Symbol = "maxi", .Description = "Maximum value operator"})
+
+        BinaryOperators.Add(New LogicalOperator With {.Name = "=", .Symbol = "=", .Description = "Equality"})
+        BinaryOperators.Add(New LogicalOperator With {.Name = "<>", .Symbol = "<>", .Description = "Inequation"})
+        BinaryOperators.Add(New LogicalOperator With {.Name = "<", .Symbol = "<", .Description = "Lower than"})
+        BinaryOperators.Add(New LogicalOperator With {.Name = ">", .Symbol = ">", .Description = "Greater than"})
+        BinaryOperators.Add(New LogicalOperator With {.Name = "<=", .Symbol = "<", .Description = "Lower or equal"})
+        BinaryOperators.Add(New LogicalOperator With {.Name = ">=", .Symbol = ">=", .Description = "Greater or equal"})
+
+    End Sub
+End Class
+Public Class LogicalOperator
+    Public Property Name As String
+    Public Property Description As String
+    Public Property Symbol As String
 End Class
---
+++ Szunyi.Math/mxparser.vb
@@ -0,0 +1,42 @@
+﻿Imports org.mariuszgromada.math.mxparser
+Public Class mxparser
+    Public Shared Function Get_Blast_Arguments(s() As String) As Dictionary(Of String, Own_Argument)
+        Dim args As New Dictionary(Of String, Own_Argument)
+        Dim UdedItems As New List(Of String)
+        For i1 = 0 To s.Count - 1
+            If s(i1).StartsWith("Hsp") Then
+                If args.ContainsKey(s(i1)) = False Then
+                    args.Add(s(i1), New Own_Argument(s(i1)))
+                End If
+            ElseIf s(i1).StartsWith("Hit") Then
+                If args.ContainsKey(s(i1)) = False Then
+                    args.Add(s(i1), New Own_Argument(s(i1)))
+                End If
+            ElseIf s(i1).StartsWith("Record") Then
+                If args.ContainsKey(s(i1)) = False Then
+                    args.Add(s(i1), New Own_Argument(s(i1)))
+                End If
+
+            End If
+
+        Next
+        Return args
+
+    End Function
+End Class
+Public Class Own_Argument
+    Public Property Name As String
+    Public Property Arg As Argument
+    Public Property Value As List(Of Double)
+    Public Property Values As List(Of Double)()
+    Public Sub New(Name As String)
+        Me.Name = Name
+        Arg = New Argument(Me.Name, 1)
+        Arg.setArgumentName(Me.Name)
+        If Arg.getArgumentName = "" Then
+            Dim jj As Int16 = 54
+        End If
+        Dim jk As Int16 = 54
+    End Sub
+
+End Class
